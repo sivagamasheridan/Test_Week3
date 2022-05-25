b8aaa25d3cfcecd0de51d064431bffdc9edbcf49
sivagamasheridan <46792094+sivagamasheridan@users.noreply.github.com>
2022-05-25 10:57:47 -0400

    Update StudentDetailsPrint.java

--- src/studentdetailsprint/StudentDetailsPrint.java
+++ src/studentdetailsprint/StudentDetailsPrint.java
@@ -32,6 +32,7 @@ public class StudentDetailsPrint {
        for(int i=0;i<list.length;i++)
        {
            System.out.println(list[i].getName() + " "+list[i].getAge());
+           System.out.println("Pull operation");
        }
        
     }

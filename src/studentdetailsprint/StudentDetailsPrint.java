d8e6002d5ddd477b5d4584c27a70010fe21db6e4
sivagamasheridan <sivagama.srinivasan@sheridanc.on.ca>
2022-05-26 09:42:56 -0400

    week 3 example version 1

--- src/studentdetailsprint/StudentDetailsPrint.java
+++ src/studentdetailsprint/StudentDetailsPrint.java
@@ -33,6 +33,7 @@ public class StudentDetailsPrint {
        {
          System.out.println(list[i].getName() + " "+list[i].getAge());
          System.out.println("Learn Github operatiions");
+        //commit and save
        }
        
     }
